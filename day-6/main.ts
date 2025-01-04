7ccc9a04fa1eb2202aa3513ff5e02c1a4d0da2b6
Clay Simmons <clay.simmons@gmail.com>
2025-01-03 20:16:26 -0600

    day 6 loop detector

--- day-6/main.ts
+++ day-6/main.ts
@@ -25,20 +25,64 @@ export const validCoord = (
   numRows: number,
 ) => (x >= 0 && x < numCols && y >= 0 && y < numRows);
 
+export const loopDetected = (edgeTips: Set<number[]>) => {
+  const coordinates = edgeTips.values().toArray();
+  if (!coordinates || coordinates.length < 2) return false;
+
+  const vectors = [];
+  // Look at start and end of edges.
+  for (let i = 0; i < coordinates.length - 1; i++) {
+    const [startX, startY] = coordinates[i];
+    const [endX, endY] = coordinates[i + 1];
+    // Vertical?
+    if (startY === endY) {
+      vectors.push([endX - startX, 0]);
+    }
+    // Horizontal?
+    if (startX === endX) {
+      vectors.push([0, endY - startY]);
+    }
+  }
+
+  let x = 0, y = 0;
+  const visitedPoints = new Set();
+  visitedPoints.add(`${x},${y}`);
+
+  // Check cumulative x,y distance traveled.
+  for (const [dx, dy] of vectors) {
+    x += dx;
+    y += dy;
+    // Have we been here before? If yes, we're in a loop.
+    if (visitedPoints.has(`${x},${y}`)) {
+      return true;
+    }
+    visitedPoints.add(`${x},${y}`);
+  }
+
+  // If cumulative distance traveled is 0, you are in a loop.
+  return x === 0 && y === 0;
+};
+
+// Traverse the grid, avoiding obstacles, and tracking visited positions,
+// as well as positions where the path turns on a 90 degree axis.
+// If traversal results in an endless loop, exit, and report that a loop condition exists.
+// Assumes we start heading north from a starting position.
 export const traverse = (
   grid: string[][],
+  startPosition: number[]
 ) => {
   const numRows = grid.length;
   const numCols = grid[0].length;
 
-  const direction = "^";
   let heading = [-1, 0]; // north
   let headingIndex = 0;
   let [headingX, headingY] = heading;
-  const position = positions(grid, direction).at(0)!;
-  let [x, y] = position;
+  let [x, y] = startPosition;
   let [nextX, nextY] = [x + headingX, y + headingY];
   const visited = new Map();
+  const edgeTips: Set<number[]> = new Set();
+  edgeTips.add([x, y]);
+  let looping = false;
   visited.set(`${x},${y}`, (visited.get(`${x},${y}`) || 0) + 1);
 
   // Until going to the next position puts us off the board...
@@ -46,6 +90,9 @@ export const traverse = (
     // Obstacle ahead?
     const nextChar = grid[nextX][nextY];
     if (nextChar === "#") {
+      // Record position where direction changed
+      edgeTips.add([x, y]);
+      looping = loopDetected(edgeTips);
       // New heading
       headingIndex = (headingIndex + 1) % 4;
       heading = Object.values(directions[headingIndex]).at(0)!;
@@ -59,14 +106,17 @@ export const traverse = (
     nextX = x + headingX;
     nextY = y + headingY;
     visited.set(`${x},${y}`, (visited.get(`${x},${y}`) || 0) + 1);
-  } while (validCoord(nextX, nextY, numCols, numRows));
-
-  return visited.size;
+  } while (validCoord(nextX, nextY, numCols, numRows) && !looping);
+  return {
+    visited: visited.size,
+    looping,
+  };
 };
 
 if (import.meta.main) {
   const strGrid = await Deno.readTextFile(Deno.args[0]);
   const grid = toGrid(strGrid);
-  const visited1 = traverse(grid);
-  console.log(`Part 1: Visited ${visited1} unique locations`);
+  const startPosition = positions(grid, "^").at(0)!;
+  const result1 = traverse(grid, startPosition);
+  console.log(`Part 1: Visited ${result1.visited} unique locations. Loop detected? ${result1.looping ? "Yes" : "No"} `);
 }
--- day-6/main_test.ts
+++ day-6/main_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "@std/assert";
-import { traverse, toGrid} from "./main.ts";
+import { traverse, toGrid, positions} from "./main.ts";
 
 const strState1 = `
 ....#.....
@@ -16,6 +16,121 @@ const strState1 = `
 
 Deno.test(function navigateTest() {
   const grid = toGrid(strState1)
-  const visitedPositions = traverse(grid)
-  assertEquals(visitedPositions, 41)
+  const startPosition = positions(grid, "^").at(0)!;
+  const result = traverse(grid, startPosition)
+  assertEquals(result.visited, 41)
+})
+
+const obstacle1 = `
+....#.....
+....+---+#
+....|...|.
+..#.|...|.
+....|..#|.
+....|...|.
+.#.#^---+.
+........#.
+#.........
+......#...
+`
+Deno.test(function loopDetectionTest1() {
+  const grid = toGrid(obstacle1)
+  const startPosition = positions(grid, "^").at(0)!;
+  const result = traverse(grid, startPosition)
+  assertEquals(result.looping, true)
+})
+
+const obstacle2 = `
+....#.....
+....+---+#
+....|...|.
+..#.|...|.
+..+-+-+#|.
+..|.|.|.|.
+.#+-^-+-+.
+......#.#.
+#.........
+......#...
+`
+Deno.test(function loopDetectionTest2() {
+  const grid = toGrid(obstacle2)
+  const startPosition = positions(grid, "^").at(0)!;
+  const result = traverse(grid, startPosition)
+  assertEquals(result.looping, true)
+})
+
+const obstacle3 = `
+....#.....
+....+---+#
+....|...|.
+..#.|...|.
+..+-+-+#|.
+..|.|.|.|.
+.#+-^-+-+.
+.+----+##.
+#+----+...
+......#...
+`
+Deno.test(function loopDetectionTest3() {
+  const grid = toGrid(obstacle3)
+  const startPosition = positions(grid, "^").at(0)!;
+  const result = traverse(grid, startPosition)
+  assertEquals(result.looping, true)
+})
+
+const obstacle4 = `
+....#.....
+....+---+#
+....|...|.
+..#.|...|.
+..+-+-+#|.
+..|.|.|.|.
+.#+-^-+-+.
+..|...|.#.
+##+---+...
+......#...
+`
+Deno.test(function loopDetectionTest4() {
+  const grid = toGrid(obstacle4)
+  const startPosition = positions(grid, "^").at(0)!;
+  const result = traverse(grid, startPosition)
+  assertEquals(result.looping, true)
+})
+
+const obstacle5 = `
+....#.....
+....+---+#
+....|...|.
+..#.|...|.
+..+-+-+#|.
+..|.|.|.|.
+.#+-^-+-+.
+....|.|.#.
+#..#+-+...
+......#...
+`
+Deno.test(function loopDetectionTest5() {
+  const grid = toGrid(obstacle5)
+  const startPosition = positions(grid, "^").at(0)!;
+  const result = traverse(grid, startPosition)
+  assertEquals(result.looping, true)
+})
+
+const obstacle6 = `
+....#.....
+....+---+#
+....|...|.
+..#.|...|.
+..+-+-+#|.
+..|.|.|.|.
+.#+-^-+-+.
+.+----++#.
+#+----++..
+......##..
+`
+Deno.test(function loopDetectionTest6() {
+  const grid = toGrid(obstacle6)
+  const startPosition = positions(grid, "^").at(0)!;
+  const result = traverse(grid, startPosition)
+  assertEquals(result.looping, true)
 })
